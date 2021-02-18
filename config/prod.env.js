e45a1f145f53777d3d0d29e185f733e454b08872
NomanHameed <nomanbutt8322@gmail.com>
2021-02-18 22:01:25 +0500

    apply envirmantal variable

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,6 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  BASE_URL: '"http://localhost:3000/"'
+  // BASE_URL: '"https://evergenius.staging.wishpond.com/"'
 }
